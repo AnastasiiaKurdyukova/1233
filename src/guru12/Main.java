13172884c488805d39d8aeb6154f6abfa6a29adc
anastasia.folomeeva <merrmait@gmail.com>
2022-04-06 12:56:08 +0300

    Added new line about slow branch

--- src/guru12/Main.java
+++ src/guru12/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println('Another line"');
+        System.out.println("Another line");
+        System.out.println("Slow branch");
     }
 }
